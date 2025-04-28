21dc8a71ffcfb03d5ad40b8464cad39a5bf4b827
mq-yuan <dearmengqiyuan@outlook.com>
2025-04-28 16:31:42 +0800

    refactor: convert fish scripts to chezmoi templates for Linux-only use
    - Rename fish completion and function files to .tmpl extensions
    
    - Add conditional checks to render content only on Linux
    
    - Adjust Neovim wrap setting to true

--- private_dot_config/nvim/lua/plugins/astrocore.lua
+++ private_dot_config/nvim/lua/plugins/astrocore.lua
@@ -41,7 +41,7 @@ return {
         number = true, -- sets vim.opt.number
         spell = false, -- sets vim.opt.spell
         signcolumn = "yes", -- sets vim.opt.signcolumn to yes
-        wrap = false, -- sets vim.opt.wrap
+        wrap = true, -- sets vim.opt.wrap
 
         expandtab = true, -- sets vim.opt.expandtab: <tab> to <space>
         tabstop = 4, -- sets vim.opt.tabstop: <tab> show 4 <space>
