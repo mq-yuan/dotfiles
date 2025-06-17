e00684ede1cedd2cda6096de35e8f0aa522dd542
mq-yuan <dearmengqiyuan@outlook.com>
2025-06-17 18:02:00 +0800

    feat: add tinymist commands to astrolsp
    - Add pin/unpin commands

--- private_dot_config/nvim/lua/plugins/astrolsp.lua
+++ private_dot_config/nvim/lua/plugins/astrolsp.lua
@@ -88,6 +88,23 @@ return {
 			    	formatterMode = "typestyle",
 			    	exportPdf = "never",
 			  	},
+			  	on_attach = function(client, bufnr)
+        			vim.keymap.set("n", "<leader>tp", function()
+            			client:exec_cmd({
+                			title = "pin",
+                			command = "tinymist.pinMain",
+                			arguments = { vim.api.nvim_buf_get_name(0) },
+            			}, { bufnr = bufnr })
+        			end, { desc = "[T]inymist [P]in", noremap = true })
+
+        			vim.keymap.set("n", "<leader>tu", function()
+            			client:exec_cmd({
+                			title = "unpin",
+                			command = "tinymist.pinMain",
+                			arguments = { vim.v.null },
+            			}, { bufnr = bufnr })
+        			end, { desc = "[T]inymist [U]npin", noremap = true })
+    			end,
 			},
 		},
 		-- customize how language servers are attached
