1e2097b3949921a75314c23c01a14e462f801567
mq-yuan <dearmengqiyuan@outlook.com>
2025-10-05 20:02:51 +0800

    docs(fish): update descriptions for `fish_reload` and `pathls` functions
    The descriptions for the `fish_reload` and `pathls` functions are updated to be more accurate and descriptive of their actual behavior.
    
    * `fish_reload` description now clearly states that it reloads the shell by replacing the current process with a new login shell (`exec $SHELL -l`).
    * `pathls` description now clearly states that it lists each directory in the `$PATH` variable on a new line.

--- private_dot_config/fish/modules/fish_functions/functions/fish_reload.fish
+++ private_dot_config/fish/modules/fish_functions/functions/fish_reload.fish
@@ -1,4 +1,4 @@
 # $HOME/.config/fish/functions/fish_reload.fish
-function fish_reload --description 'reload the fish all config include `config.fish` and other files(such as `conf.d/`)' 
+function fish_reload --description 'Reloads the shell by replacing the current process with a new login shell' 
   exec $SHELL -l
 end
--- private_dot_config/fish/modules/fish_functions/functions/pathls.fish
+++ private_dot_config/fish/modules/fish_functions/functions/pathls.fish
@@ -1,5 +1,5 @@
 # $HOME/.config/fish/functions/pathls.fish
-function pathls
+function pathls --description 'Lists each directory in the $PATH variable on a new line'
     string split : $PATH
     return 0
 end
