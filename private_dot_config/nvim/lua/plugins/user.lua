1c821324db6b74f088e2e62f44b816605dfb5269
mq-yuan <dearmengqiyuan@outlook.com>
2025-10-19 19:50:02 +0800

    refactor(nvim): comment out default AstroNvim LuaSnip configuration
    The default configuration was commented out because it was interfering with
    the custom snippet loading logic, causing snippets to not load correctly.
    
    style(nvim): comment out default dependency in Python script snippet
    
    The default dependency was commented out to provide a cleaner template
    for new Python scripts, requiring the user to explicitly define
    dependencies if needed.

--- private_dot_config/nvim/lua/plugins/user.lua
+++ private_dot_config/nvim/lua/plugins/user.lua
@@ -143,7 +143,8 @@ return {
   {
     "L3MON4D3/LuaSnip",
     config = function(plugin, opts)
-      require "astronvim.plugins.configs.luasnip"(plugin, opts) -- include the default astronvim config that calls the setup call
+      -- include the default astronvim config that calls the setup call
+      -- require "astronvim.plugins.configs.luasnip"(plugin, opts)
       -- load snippets paths: ~/.config/nvim/snippets
       -- :lua vim.print(vim.fn.stdpath("config")) -> ~/.config/nvim
       require("luasnip.loaders.from_vscode").lazy_load({
