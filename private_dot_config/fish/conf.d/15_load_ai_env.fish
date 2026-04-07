77eeab0713d1d20d1de24c9be49b965987b79226
mq-yuan <dearmengqiyuan@outlook.com>
2026-04-07 18:10:52 +0800

    feat(claude-code): comment KIMI URL for claude

--- private_dot_config/fish/conf.d/15_load_ai_env.fish
+++ private_dot_config/fish/conf.d/15_load_ai_env.fish
@@ -14,7 +14,7 @@ set --global --export XAI_API_KEY $XAI_API_KEY
 # -- DeepSeek --
 set --global --export DEEPSEEK_API_KEY $DEEPSEEK_API_KEY
 
-# -- Anthropic/Claude (or any tool that needs this key) --
-set --global --export ANTHROPIC_BASE_URL $KIMI_BASE_URL_FOR_ANTHROPIC
-set --global --export ANTHROPIC_API_KEY $KIMI_API_KEY
-
+# # -- Anthropic/Claude (or any tool that needs this key) --
+# set --global --export ANTHROPIC_BASE_URL $KIMI_BASE_URL_FOR_ANTHROPIC
+# set --global --export ANTHROPIC_API_KEY $KIMI_API_KEY
+#
