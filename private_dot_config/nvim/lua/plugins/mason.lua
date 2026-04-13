f42ba93e80a3ec66d17828b650497a4e9838eb0e
mq-yuan <dearmengqiyuan@outlook.com>
2026-04-13 18:56:08 +0800

    chore(mason): remove tree-sitter-cli from auto-installed packages
    Removes `tree-sitter-cli` from the `ensure_installed` list in Mason.
    It is no longer necessary to manage `tree-sitter-cli` via Mason.
    This avoids redundant installations and potential conflicts.

--- private_dot_config/nvim/lua/plugins/mason.lua
+++ private_dot_config/nvim/lua/plugins/mason.lua
@@ -2,31 +2,28 @@
 
 ---@type LazySpec
 return {
-  -- use mason-tool-installer for automatically installing Mason packages
-  {
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-    -- overrides `require("mason-tool-installer").setup(...)`
-    opts = {
-      -- Make sure to use the names found in `:Mason`
-      ensure_installed = {
-        -- install language servers
-        "yaml-language-server",
-        "lua-language-server",
-        "ruff",
-        "rust-analyzer",
-        "basedpyright",
-        "clangd",
-        "tinymist",
+	-- use mason-tool-installer for automatically installing Mason packages
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		-- overrides `require("mason-tool-installer").setup(...)`
+		opts = {
+			-- Make sure to use the names found in `:Mason`
+			ensure_installed = {
+				-- install language servers
+				"yaml-language-server",
+				"lua-language-server",
+				"ruff",
+				"rust-analyzer",
+				"basedpyright",
+				"clangd",
+				"tinymist",
 
-        -- install formatters
-        "stylua",
+				-- install formatters
+				"stylua",
 
-        -- install debuggers
-        "debugpy",
-
-        -- install any other package
-        "tree-sitter-cli",
-      },
-    },
-  },
+				-- install debuggers
+				"debugpy",
+			},
+		},
+	},
 }
