e1d9e4e6d9cb098cc33d0d78be33b692924edf43
mq-yuan <dearmengqiyuan@outlook.com>
2026-04-20 11:59:16 +0800

    feat(nvim): add soft warp

--- private_dot_config/nvim/lua/polish.lua
+++ private_dot_config/nvim/lua/polish.lua
@@ -1,5 +1,13 @@
-if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- This will run last in the setup process.
 -- This is just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
+
+-- Enable soft wrap, linebreak, and indent preservation for markdown files
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "markdown",
+	callback = function()
+		vim.opt_local.wrap = true
+		vim.opt_local.linebreak = true
+		vim.opt_local.breakindent = true
+	end,
+})
