bab09a9fd5083ebad577b6d7da2fcfb0566835a6
mq-yuan <dearmengqiyuan@outlook.com>
2026-09-03 12:09:58 +0800

    chore: drop skim; use vimtex general viewer

--- private_dot_config/nvim/lua/plugins/latex.lua
+++ private_dot_config/nvim/lua/plugins/latex.lua
@@ -3,7 +3,7 @@ return {
 	lazy = false,
 	ft = { "tex", "bib" },
 	init = function()
-		vim.g.vimtex_view_method = "skim"
+		vim.g.vimtex_view_method = "general"
 		vim.g.vimtex_compiler_method = "latexmk"
 	end,
 }
